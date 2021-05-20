2c2fa54354272a57964f453be14e1d02877231e2
Mehmet GUNEY <guneym@sheridancollege.ca>
2021-05-20 13:48:07 -0400

    create first project on IntelliJ

---
+++ src/test/java/sheridan/guneym/hellosheridan/HelloSheridanApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.guneym.hellosheridan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSheridanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
