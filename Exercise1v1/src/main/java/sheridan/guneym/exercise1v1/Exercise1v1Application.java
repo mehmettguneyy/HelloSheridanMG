a8c2fe596428492ee99a0bbdca2980c8f4494bc6
Mehmet GUNEY <guneym@sheridancollege.ca>
2021-05-29 21:53:36 -0400

    added Index.html instead of Input.html committed application.properties and Exercise1v1Application

---
+++ Exercise1v1/src/main/java/sheridan/guneym/exercise1v1/Exercise1v1Application.java
@@ -0,0 +1,13 @@
+package sheridan.guneym.exercise1v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1v1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise1v1Application.class, args);
+    }
+
+}
