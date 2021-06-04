a12e263f03662419ee675ec6dfe9b6132b14e0fb
Mehmet GUNEY <guneym@sheridancollege.ca>
2021-06-03 22:39:30 -0400

    added input page to radios and formula for game.java

--- assignment1/src/main/java/sheridan/guneym/assignment1/domain/Game.java
+++ assignment1/src/main/java/sheridan/guneym/assignment1/domain/Game.java
@@ -1,4 +1,22 @@
 package sheridan.guneym.assignment1.domain;
 
+
+
 public class Game {
-}
+
+    private final static String[] INPUT
+            = {"Rock", "Paper", "Scissor"};
+
+    final private int number;
+    final private String name;
+
+
+        public Game() {
+            int rand = (int) (3 * Math.random());
+            name = INPUT[rand / 3];
+            number = rand + 1;
+        }
+
+     }
+
+
